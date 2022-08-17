7e9ecb0ef326e9cb6e56674f86d6a78064417794
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-08-17 19:00:16 +0000

    Altering generate_schema_name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -5,7 +5,7 @@
     {% do log(log_msg, False) %}
 
     {%- if custom_schema_name -%}
-        {{ custom_schema_name if 'qa' in target.name.lower() else target.schema ~ '__' ~ custom_schema_name }}
+        {{ target.schema if 'dev' in target.name.lower() else custom_schema_name }}
     {%- else -%}
         {{ default_schema }} 
     {%- endif -%}
