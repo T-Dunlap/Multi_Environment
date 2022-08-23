bfc110b6726a09c93cfbdee0d9e4266361a8eac3
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2022-08-23 23:22:56 +0000

    removing comments

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,12 +1,3 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
---Adding comment to prompt change
 {{ config(materialized='table') }}
 
 with source_data as (
@@ -24,8 +15,4 @@ with source_data as (
 select *
 from source_data
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
 
--- where id is not null
