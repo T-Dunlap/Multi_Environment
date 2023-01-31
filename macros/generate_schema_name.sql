1f4fb2813a1fe3d27baa9681a640675275c94539
Taylor Dunlap <tayjdunlap@gmail.com>
2023-01-31 07:24:13 -0800

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,13 +1,27 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    
-    {% set log_msg='getting custom schema:\ntarget_name:' ~ target.name ~ '\ncustom_schema_name:' ~ custom_schema_name %}
-    {% do log(log_msg, False) %}
 
-    {%- if custom_schema_name -%}
-        {{ target.schema if 'dev' in target.name.lower() else custom_schema_name }}
+    {%- set default_schema = target.schema -%}
+ {#    {% if not custom_schema_name and node.resource_type == 'model' %}
+        {{ exceptions.raise_compiler_error(
+            'Model must have a custom schema configured.'
+        )}}
+    {% endif%}
+#}
+    {% set default_dev_schema =  env_var("DBT_DEV_TARGET_SCHEMA", 'DEV') %}
+    {% set default_ci_schema =  env_var("DBT_CI_TARGET_SCHEMA", 'CI') %}
+
+    {%- if target.name == 'dev' -%}
+
+        {{ default_dev_schema }}
+
+    {%- elif target.name == 'ci' -%}
+
+        {{ default_ci_schema }}
+
     {%- else -%}
-        {{ default_schema }} 
+
+        {{ custom_schema_name | trim }}
+
     {%- endif -%}
 
 {%- endmacro %}
